3c44e16c04ca35b2120b2d97efcdff86cd9e123a
Thorsten Göckeler <thorsten@goeckeler.com>
2019-07-07 22:02:40 +0200

    Added a grid with multiple solutions.

--- projecteuler.net/Euler/src/euler/p096/TestSudoku.java
+++ projecteuler.net/Euler/src/euler/p096/TestSudoku.java
@@ -8,25 +8,56 @@ import java.util.List;
  */
 public class TestSudoku
 {
-  public static final String[] test =
-      {
-        "003020600", "900305001", "001806400", "008102900", "700000008", "006708200", "002609500", "800203009",
-        "005010300"
-      };
+  public static final String[] test = {
+    "003020600",
+    "900305001",
+    "001806400",
+
+    "008102900",
+    "700000008",
+    "006708200",
+
+    "002609500",
+    "800203009",
+    "005010300"
+  };
 
   public static final String[] pressewoche = {
-    "530001000", "900040000", "080009700", "050010006",
-    "706050308", "300080040", "003200060", "000030002",
+    "530001000",
+    "900040000",
+    "080009700",
+
+    "050010006",
+    "706050308",
+    "300080040",
+
+    "003200060",
+    "000030002",
     "000700093"
   };
-  
+
+  public static final String[] multipleSolutions = {
+    "007060002",
+    "080900034",
+    "010050900",
+
+    "050010600",
+    "040008070",
+    "002390080",
+
+    "006040010",
+    "500007040",
+    "400030700"
+  };
+
   public static void main(final String[] args) {
     show(test);
     show(pressewoche);
+    show(multipleSolutions);
   }
-  
-  public static void show(String[] puzzle) {
-    Sudoku sudoku = new Sudoku();
+
+  public static void show(final String[] puzzle) {
+    final Sudoku sudoku = new Sudoku();
     for (int row = 0; row < puzzle.length; ++row) {
       sudoku.set(row, puzzle[row]);
     }
@@ -34,8 +65,8 @@ public class TestSudoku
     System.out.println("\n=SUDOKU==\n");
     System.out.println(sudoku.toString());
     System.out.println("=========\n");
-    List<Grid> solved = sudoku.solve();
-    for (Grid grid : solved) {
+    final List<Grid> solved = sudoku.solve();
+    for (final Grid grid : solved) {
       System.out.println(grid.toString());
     }
   }
